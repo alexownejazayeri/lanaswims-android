730fe4e9e3fc241564a667b755a3df5ed11fcef7
Alex Ownejazayeri <alirezaownejazayeri@gmail.com>
2025-04-06 17:15:22 -0400

    adds primitive placeholder card for tide chart viz

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.patrykandpatrick.vico:compose:1.10.0") // Jetpack Compose charts
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.10.1")
--- app/src/main/java/com/eloftee/lanaswims/ui/LanaSwimsApp.kt
+++ app/src/main/java/com/eloftee/lanaswims/ui/LanaSwimsApp.kt
@@ -3,10 +3,13 @@ package com.eloftee.lanaswims.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,13 +18,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.eloftee.lanaswims.R
+import com.patrykandpatrick.vico.compose.chart.Chart
+import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.core.entry.entryModelOf
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LanaSwimsApp() {
+
+
     Scaffold(
         topBar = {
             TopAppBar(title = { HeaderTitle() })
@@ -32,9 +42,44 @@ fun LanaSwimsApp() {
                 .fillMaxSize()
                 .padding(padding)
                 .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Today's Spot Summary")
+            Text(
+                text = "Berkeley Marina Tide Chart",
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Bold,
+                lineHeight = 36.sp,
+                letterSpacing = 0.5.sp,
+                fontSize = 32.sp,
+                modifier = Modifier
+                    .width(256.dp)
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "Sunday, April 6",
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(216.dp)
+
+            ) {
+                Chart(
+                    chart = lineChart(),
+                    model = entryModelOf(*listOf(1.2f, 2.5f, 3.0f, 4.1f, 2.8f).toTypedArray()),
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            }
+
+
         }
     }
 }
